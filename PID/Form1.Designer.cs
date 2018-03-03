51adaf4ae2a4a3d38ab364b5ff08f2d5a6793cc2
tjharrison1 <tjharrison1@CM125-2017-12>
2018-03-03 20:35:30 +0000

    Add project files.

---
+++ PID/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace PID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Vin = new System.Windows.Forms.TextBox();
+            this.Ind = new System.Windows.Forms.TextBox();
+            this.Cap = new System.Windows.Forms.TextBox();
+            this.Load = new System.Windows.Forms.TextBox();
+            this.Vout = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.Name = "Series2";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(1055, 540);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(895, 558);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 102);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Vin
+            // 
+            this.Vin.Location = new System.Drawing.Point(12, 629);
+            this.Vin.Name = "Vin";
+            this.Vin.Size = new System.Drawing.Size(112, 31);
+            this.Vin.TabIndex = 2;
+            // 
+            // Ind
+            // 
+            this.Ind.Location = new System.Drawing.Point(130, 629);
+            this.Ind.Name = "Ind";
+            this.Ind.Size = new System.Drawing.Size(112, 31);
+            this.Ind.TabIndex = 3;
+            // 
+            // Cap
+            // 
+            this.Cap.Location = new System.Drawing.Point(248, 629);
+            this.Cap.Name = "Cap";
+            this.Cap.Size = new System.Drawing.Size(112, 31);
+            this.Cap.TabIndex = 4;
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(366, 629);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(112, 31);
+            this.Load.TabIndex = 5;
+            // 
+            // Vout
+            // 
+            this.Vout.Location = new System.Drawing.Point(484, 629);
+            this.Vout.Name = "Vout";
+            this.Vout.Size = new System.Drawing.Size(112, 31);
+            this.Vout.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 601);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Vin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 601);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Inductor (H)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 601);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Capacitor (f)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(366, 601);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Load (Ω)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(479, 601);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Vout";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 672);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Vout);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Cap);
+            this.Controls.Add(this.Ind);
+            this.Controls.Add(this.Vin);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Vin;
+        private System.Windows.Forms.TextBox Ind;
+        private System.Windows.Forms.TextBox Cap;
+        private System.Windows.Forms.TextBox Load;
+        private System.Windows.Forms.TextBox Vout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ PID/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PID
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Vin.Text = "24";
+            Ind.Text = "0.0006";
+            Cap.Text = "0.00033";
+            Load.Text = "7";
+            Vout.Text = "70";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+
+            chart1.Series.Add("Ton * 100");
+            chart1.Series["Ton * 100"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart1.Series.Add("Vout");
+            chart1.Series["Vout"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart1.Series.Add("Toff * 100");
+            chart1.Series["Toff * 100"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            double
+                vin = Convert.ToDouble(Vin.Text),
+                ind = Convert.ToDouble(Ind.Text),
+                cap = Convert.ToDouble(Cap.Text),
+                load = Convert.ToDouble(Load.Text),
+                vout = Convert.ToDouble(Vout.Text),
+                ton, toff, freq = 0.000033, tontoff,
+                Noise = 0, posNeg, setpoint = vout, error=0,
+                Prop, Inte, Dir,  errSig = 0, PID, errPrev;
+
+            Random noise = new Random();
+
+            for (int i = 0; i < 999; i++)
+            {
+                tontoff = vout / vin;
+                ton = tontoff / (1 + tontoff);
+                toff = ton - freq;
+
+                chart1.Series["Ton * 100"].Points.AddXY(i, (ton*100));
+                chart1.Series["Vout"].Points.AddXY(i, vout);
+                chart1.Series["Toff * 100"].Points.AddXY(i, toff*100);
+
+                posNeg = noise.Next(0, 101);
+                if((posNeg > 50) && ((vout/10) >1))
+                {
+                    Noise = -(noise.Next(0, Convert.ToInt32(vout / 10)));
+                }
+                else if ((vout / 10) > 1)
+                {
+                    Noise = noise.Next(0, Convert.ToInt32(vout / 10));
+                }
+                errPrev = error;
+                vout += Noise;
+                error = vout - setpoint;
+                errSig += error;
+                Prop = 0.75*-error;
+                if (i == 0)
+                {
+                    Inte = 0.08 * (errSig);
+                }
+                else
+                {
+                    Inte = 0.4 * (errSig / i);
+                }
+                Dir = 0.13 * (errPrev - error);
+                PID = Prop + Inte + Dir;
+                vout += PID;
+            }
+        }
+    }
+}
